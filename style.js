3eb584e60cfdb6835e5e791072b17bd7b48da4d1
Yasine Lallmahomed <ylallmahomed@ps360.co.uk>
2025-06-20 23:55:14 +0000

    Clean up

--- style.js
+++
@@ -1,9 +0,0 @@
-// Navbar background opacity on scroll
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 100) {
-        navbar.style.background = 'rgba(245, 249, 245, 0.98)';
-    } else {
-        navbar.style.background = 'rgba(245, 249, 245, 0.95)';
-    }
-});
